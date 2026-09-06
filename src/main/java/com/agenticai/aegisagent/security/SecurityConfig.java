e9c74f3ec9c174cf01abe3e7d4ca30c0e14fa9b3
Rohitha Ponnapalli <rohithaponnapalli25@gmail.com>
2026-09-06 15:39:16 +0530

    aegis

--- src/main/java/com/agenticai/aegisagent/security/SecurityConfig.java
+++ src/main/java/com/agenticai/aegisagent/security/SecurityConfig.java
@@ -24,7 +24,8 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(
                 List.of(
-                        "http://localhost:5173"
+                        "http://localhost:5173",
+                        "https://aegis-avenger.vercel.app"
                 )
         );
 
